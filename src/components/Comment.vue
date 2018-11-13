674980afc2c7f2d48382b0a8099752d9e1d53da2
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2018-11-14 00:52:29 +0100

    Filter comments

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,43 +1,39 @@
 <template>
-  <div id="comment">
-    <div class="block">
-      <div class="text">
-        <p>{{ content }}</p>
-        <p class="name">{{ name }}</p>
-      </div>
-    </div>
-  </div>
+	<div id="comment">
+		<div class="block">
+			<div class="text">
+				<p>{{ content }}</p>
+				<p class="name">{{ name }}</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 	export default {
 		name: "comment",
-    props: ['amount', 'name', 'content'],
-    methods: {
-
-    }
+		props: ['amount', 'name', 'content']
 	}
 </script>
 
 <style scoped>
-  .block {
-    width: 80%;
-    height: 100%;
-    margin-top: 5%;
-    padding: 10px;
-    transition: box-shadow 0s;
-    overflow: hidden;
-    transform: scale(1.03);
-    box-sizing: border-box;
-    background-size: cover;
-    box-shadow: 0 8px 6px -6px #979797;
-    background-color: #f8f9fa;
-  }
-  .name {
-    font-style: italic;
-  }
-  .text{
-    text-align: left;
-  }
-
+	.block {
+		width: 80%;
+		height: 100%;
+		margin-top: 5%;
+		padding: 10px;
+		transition: box-shadow 0s;
+		overflow: hidden;
+		transform: scale(1.03);
+		box-sizing: border-box;
+		background-size: cover;
+		box-shadow: 0 8px 6px -6px #979797;
+		background-color: #f8f9fa;
+	}
+	.name {
+		font-style: italic;
+	}
+	.text{
+		text-align: left;
+	}
 </style>
